a8e87319e5926c3b54082a370e63ce37dff8feed
boopathydreams <boopathy.nr@gmail.com>
2025-09-16 18:18:44 +0530

    refactor: simplify Input component props definition by removing unused interface

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,12 +1,7 @@
 import { forwardRef } from 'react'
 import { cn } from '@/lib/utils'
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {
-  // Additional props can be added here in the future
-}
-
-const Input = forwardRef<HTMLInputElement, InputProps>(
+const Input = forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
   ({ className, type, ...props }, ref) => {
     return (
       <input
